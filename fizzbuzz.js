ae8ba0b8d87354dd3d5597d0da4b38c669ddb9d9
Karanja-JK <65297241+Karanja-JK@users.noreply.github.com>
2020-05-13 17:46:51 +0300

    Add files via upload

---
+++ fizzbuzz.js
@@ -0,0 +1,27 @@
+var j;
+
+for(j = 1; j<= 100; j++)
+{
+    if ( j%3 === 0 && j%5 === 0 )
+    {
+        console.log('FizzBuzz');
+    }
+
+    else if( j%3 === 0)
+    {
+        console.log('Fizz');
+    }
+
+    else if( j%5 === 0)
+    {
+        console.log('Buzz');
+    }
+
+    else
+    {
+        console.log(j);
+    }
+
+    
+}
+
